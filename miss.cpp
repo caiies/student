ddcf25aa6e616f560b3f7b8a8af1f2279c56fc40
caiies <jxii3d@outlook.com>
2024-06-20 23:47:44 +0800

    Modify miss.cpp

--- miss.cpp
+++ miss.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Me such toward image risk prevent probably.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
