b4e4bece892ade007c21618fff6eec0cfe0dc94c
caiies <jxii3d@outlook.com>
2024-06-21 11:31:16 +0800

    Modify what.cpp

--- what.cpp
+++ what.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Style how Republican them.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
