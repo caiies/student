f52f5b427b4d892c2a61a7737817878cffe7a779
caiies <jxii3d@outlook.com>
2024-07-08 11:48:26 +0800

    Modify low.cpp

--- low.cpp
+++ low.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Effort different represent activity.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
