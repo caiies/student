6b3ac813f4a2bfe572a2473a71b028749ad674c0
caiies <jxii3d@outlook.com>
2024-07-11 18:09:03 +0800

    Modify car.cpp

--- car.cpp
+++ car.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Issue camera including.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
