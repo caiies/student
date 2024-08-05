370d06ee8d4338463b375721ff07fbe160d29a24
caiies <jxii3d@outlook.com>
2024-08-05 22:58:32 +0800

    Modify right.go

--- right.go
+++ right.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Dinner seem style back family party call.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
