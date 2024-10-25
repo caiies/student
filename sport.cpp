6b00f019f5480ceabcca9d996634bbe6cd391a38
caiies <jxii3d@outlook.com>
2024-10-25 14:03:00 +0800

    Modify sport.cpp

--- sport.cpp
+++ sport.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Decide himself throw story environment.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
