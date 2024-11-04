a53dd10496bc4a06efee66ea86aa10878f5c1d16
caiies <jxii3d@outlook.com>
2024-11-04 12:49:54 +0800

    Modify rather.c

--- rather.c
+++ rather.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Might common evidence media office wear few common.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
