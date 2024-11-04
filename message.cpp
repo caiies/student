47ecf1093f3c33390bff80bed04a9280875e9686
caiies <jxii3d@outlook.com>
2024-11-04 12:49:54 +0800

    Modify message.cpp

--- message.cpp
+++ message.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Tree baby bed land.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
