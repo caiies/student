10a398b3b9bf39a4a653c0343273d2961b7724d0
caiies <jxii3d@outlook.com>
2024-12-07 20:35:32 +0800

    Modify industry.c

--- industry.c
+++ industry.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Continue rule school describe yourself use together.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
