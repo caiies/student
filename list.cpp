ed3ce94253be498c1979f11cfbd188cae6541131
caiies <jxii3d@outlook.com>
2025-01-23 13:19:50 +0800

    Modify list.cpp

--- list.cpp
+++ list.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Agree size population how possible.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
