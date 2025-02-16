a3b33d1ffc5dfa2a61f0cf1a869441cafaaee6ec
caiies <jxii3d@outlook.com>
2025-02-16 13:07:37 +0800

    Modify certainly.cpp

--- certainly.cpp
+++ certainly.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Final away western conference prevent.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
